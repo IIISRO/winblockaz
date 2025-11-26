b87ee59c9924d506c025622122520a06a9900926
root <lamanmirizada@gmail.com>
2025-11-26 10:16:58 +0000

    initial commit

--- winblockRU/settings.py
+++ winblockRU/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rxkj2jrunw(u=cb8pe8@+&%*0el+rlp3@oie_e!9fzk03k8t62
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') else True 
-# DEBUG=True
+#DEBUG=True
 
 
 ALLOWED_HOSTS = ['*']
@@ -222,6 +222,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'storebyglasstech@gmail.com'
-EMAIL_HOST_PASSWORD = 'ksxdqywvxgdurjgi'
+EMAIL_HOST_USER = 'awinblock@gmail.com'
+EMAIL_HOST_PASSWORD = 'apixuennhrqwmdfd'
 
